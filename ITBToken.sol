eb0862005ab38842f86d504211b1a81cb3aa2f21
TommasoBranchi <103031895+TommasoBranchi@users.noreply.github.com>
2022-04-05 11:43:52 +0200

    Update ITBToken.sol

--- ITBToken.sol
+++ ITBToken.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.7.1;
 
-interface ITBToken
+interface IERC20Token
 {
   function totalsupply() external view returns (uint256);
   function balanceOf(address account) external view returns (uint256);
